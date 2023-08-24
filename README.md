444395b36637fe7f8019e99a0a5ad812434a7c9c
Sameer Shaikh <shaikhs975@gmail.com>
2023-08-24 23:54:07 +0530

    create first ever cli in my life feeling amazig

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sameer-general-cli
 exploring and learning to build interactive cli tools
+
+steps to use this 
+- Take a clone of this repo
+- run this command `sudo npm install -g`
---
+++ index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+/**
+ *  this is called shebang commad this is tell os to which interpreter to use
+ * in our case is node and the location where node is installed. 
+ */
+console.log(`Sameer's genie at your seevice sir!`)
